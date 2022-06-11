5c89dd6f1859abef8f80be34f79de5dddcf4b7ac
voller <voller.silas@gmail.com>
2022-06-11 14:52:09 -0300

    funcionalidade de marcar e desmarcar prato

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ let prato;
 let bebida;
 let sobremesa;
 
-let precoP;
-let precoB;
-let precoS;
+let precoPrato;
+let precoBebida;
+let precoSobremesa;
+
+function selecionaPrato(e) {
+  precoPrato = document.querySelector(".preco").innerHTML;
+  precoPrato = trataPreco(precoPrato);
+  prato = document.querySelector(".nomePrato").innerHTML;
+  console.log(prato);
+
+  let pratoSelecionado = document.querySelector(".selecionado");
+  console.log(pratoSelecionado);
+  if (pratoSelecionado !== null) {
+    pratoSelecionado.classList.toggle("selecionado");
+  }
+  e.classList.toggle("selecionado");
+}
+
+function trataPreco(v) {
+  v = v.replace("R$ ", "").replace(",", ".");
+  v = Number(v);
+  return v;
+}
